690fa93ee70b3186fdaeeafe5e90e29c19e17d9e
Pedro Calado <pdcalado@gmail.com>
2023-03-09 21:32:49 +0000

    Place exported code into pkg.

---
+++ pkg/convert.go
@@ -0,0 +1,119 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func toKebabCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}-${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}-${2}")
+	return strings.ToLower(snake)
+}
+
+func convertInputs(
+	args []string,
+	types []reflect.Type,
+) ([]reflect.Value, error) {
+
+	values := make([]reflect.Value, len(args))
+
+	for i, arg := range args {
+		value, err := convertInput(arg, types[i])
+		if err != nil {
+			return nil, err
+		}
+		values[i] = value
+	}
+
+	return values, nil
+}
+
+func convertInput(arg string, ty reflect.Type) (reflect.Value, error) {
+	value := reflect.New(ty).Elem()
+
+	switch ty.Kind() {
+	case reflect.String:
+		value.SetString(arg)
+	case reflect.Int:
+		i, err := strconv.ParseInt(arg, 10, 64)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		value.SetInt(i)
+	case reflect.Bool:
+		b, err := strconv.ParseBool(arg)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		value.SetBool(b)
+	case reflect.Slice:
+		if ty.Elem() == reflect.TypeOf(byte(0)) {
+			value.SetBytes([]byte(arg))
+			break
+		}
+
+		obj := reflect.New(ty).Interface()
+		err := json.Unmarshal([]byte(arg), obj)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		value.Set(reflect.ValueOf(obj).Elem())
+	case reflect.Struct:
+		obj := reflect.New(ty).Interface()
+		err := json.Unmarshal([]byte(arg), obj)
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		value.Set(reflect.ValueOf(obj).Elem())
+	default:
+		panic("not supported")
+	}
+
+	return value, nil
+}
+
+func outputResults(
+	obj interface{},
+	results []reflect.Value,
+) error {
+	var toPrint []string
+
+	for _, result := range results {
+		// check if a result is an error
+		if result.Type() == reflect.TypeOf((*error)(nil)).Elem() {
+			if !result.IsNil() {
+				return result.Interface().(error)
+			}
+			continue
+		}
+
+		// convert result to json
+		buf, err := json.Marshal(result.Interface())
+		if err != nil {
+			return fmt.Errorf("failed to marshal result: %s", err)
+		}
+
+		toPrint = append(toPrint, string(buf))
+	}
+
+	// if no results, print the object
+	if len(toPrint) == 0 {
+		buf, _ := json.Marshal(obj)
+		fmt.Println(string(buf))
+	}
+
+	// print results
+	for _, str := range toPrint {
+		fmt.Println(str)
+	}
+
+	return nil
+}
---
+++ pkg/create_command.go
@@ -0,0 +1,163 @@
+package pkg
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+type CreateCommandOptions struct {
+	// TypeName is the name of the type to generate a command for
+	TypeName string
+	// CommandName is the name of the command to generate
+	// (if empty, we use the type's name converted to kebab-case)
+	CommandName string
+	// Constructors is a map of functions that return objects of target type
+	Constructors map[string]reflect.Value
+	// MethodArgs is a map of method names to list of argument names
+	MethodArgs map[string][]string
+	// MethodDocs is a map of method names to documentation strings
+	MethodDocs map[string]string
+}
+
+func CreateCommand(
+	targetType reflect.Type,
+	options *CreateCommandOptions,
+) (*cobra.Command, error) {
+
+	commandName := options.CommandName
+	if options.CommandName == "" {
+		commandName = toKebabCase(options.TypeName)
+	}
+
+	methodArgs := options.MethodArgs
+	methodDocs := options.MethodDocs
+
+	typeCmd := &cobra.Command{
+		Use:   commandName,
+		Short: "",
+	}
+
+	pointerType := reflect.PtrTo(targetType)
+
+	for i := 0; i < pointerType.NumMethod(); i++ {
+		method := pointerType.Method(i)
+
+		var callArgTypes []reflect.Type
+		for j := 1; j < method.Type.NumIn(); j++ {
+			callArgTypes = append(callArgTypes, method.Type.In(j))
+		}
+
+		argNames, ok := methodArgs[method.Name]
+		if !ok {
+			return nil, fmt.Errorf("missing arg names for method '%s'", method.Name)
+		}
+
+		if len(argNames) != len(callArgTypes) {
+			return nil, fmt.Errorf("wrong number of arg names for method '%s'", method.Name)
+		}
+
+		description, hasDoc := methodDocs[method.Name]
+		if !hasDoc {
+			return nil, fmt.Errorf("missing doc for method '%s', add comment and generate again", method.Name)
+		}
+
+		subCmd := &cobra.Command{
+			Use:   toKebabCase(method.Name) + " " + strings.Join(argNames, " "),
+			Short: description,
+			Long:  description,
+			Args:  cobra.ExactArgs(len(argNames)),
+			RunE:  methodCommandRunner(targetType, method.Func, callArgTypes),
+		}
+		typeCmd.AddCommand(subCmd)
+	}
+
+	// add constructors
+	for name, constructor := range options.Constructors {
+		constructorType := constructor.Type()
+
+		var callArgTypes []reflect.Type
+		for j := 1; j < constructorType.NumIn(); j++ {
+			callArgTypes = append(callArgTypes, constructorType.In(j))
+		}
+
+		argNames, ok := methodArgs[name]
+		if !ok {
+			return nil, fmt.Errorf("missing arg names for method '%s'", name)
+		}
+
+		if len(argNames) != len(callArgTypes) {
+			return nil, fmt.Errorf("wrong number of arg names for method '%s'", name)
+		}
+
+		description, hasDoc := methodDocs[name]
+		if !hasDoc {
+			return nil, fmt.Errorf("missing doc for method '%s', add comment and generate again", name)
+		}
+
+		subCmd := &cobra.Command{
+			Use:   toKebabCase(name) + " " + strings.Join(argNames, " "),
+			Short: description,
+			Long:  description,
+			Args:  cobra.ExactArgs(len(argNames)),
+			RunE:  constructorCommandRunner(targetType, constructor, callArgTypes),
+		}
+		typeCmd.AddCommand(subCmd)
+	}
+
+	return typeCmd, nil
+}
+
+func methodCommandRunner(
+	targetType reflect.Type,
+	method reflect.Value,
+	callArgTypes []reflect.Type,
+) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		callArgs, err := convertInputs(args, callArgTypes)
+		if err != nil {
+			return fmt.Errorf("failed to convert args: %s", err)
+		}
+
+		object := reflect.New(targetType).Interface()
+
+		// check if data is being piped from stdin
+		stat, _ := os.Stdin.Stat()
+		if (stat.Mode() & os.ModeCharDevice) == 0 {
+			dec := json.NewDecoder(os.Stdin)
+			err := dec.Decode(object)
+			if err != nil {
+				return err
+			}
+		}
+
+		allArgs := append([]reflect.Value{reflect.ValueOf(object)}, callArgs...)
+
+		result := method.Call(allArgs)
+
+		return outputResults(object, result)
+	}
+}
+
+func constructorCommandRunner(
+	targetType reflect.Type,
+	method reflect.Value,
+	callArgTypes []reflect.Type,
+) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		callArgs, err := convertInputs(args, callArgTypes)
+		if err != nil {
+			return fmt.Errorf("failed to convert args: %s", err)
+		}
+
+		object := reflect.New(targetType).Interface()
+
+		result := method.Call(callArgs)
+
+		return outputResults(object, result)
+	}
+}
